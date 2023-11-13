d54600ff9d4b0276a2166fae6090e96d7d7b6edb
Ninto7 <134883217+Ninto7@users.noreply.github.com>
2023-11-13 17:50:33 +0100

    Scene manager und boss 1 scene

---
+++ My project/Assets/Loader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+     
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            LoadLevel(1);
+        }
+    }
+
+    public void LoadLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
